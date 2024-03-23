966d8d7687c40e005ef1f42e8236d500710b9a01
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2024-03-23 19:03:02 +0000

    working with the refresher section

--- refresher/code.py
+++ refresher/code.py
@@ -9,4 +9,9 @@ def age_to_months():
 
 if __name__ == '__main__':
     print('hello')
-    age_to_months()
+    # age_to_months()
+    # 'create a tuple'
+    # my_tuple = (1, 2, 3)
+    # print(my_tuple)
+    # my_tuple = (4,)
+    # print(my_tuple)
